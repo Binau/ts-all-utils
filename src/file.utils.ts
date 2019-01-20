9044dd246e721dcef014649b1f0406f31300a096
binau <aubin.roumillac@gmail.com>
2019-01-20 21:13:05 +0100

    Gestion file => json et inversement

--- src/file.utils.ts
+++ src/file.utils.ts
@@ -1,9 +1,26 @@
 import * as FS from 'fs';
 import {StringUtils} from './string.utils';
 
+/**
+ * Classe permettant de gerer les fichiers
+ */
 export class FileUtils {
 
-    public static loadJson(fileUrl: string, options: { encoding: string; flag?: string; } | string) {
-        FS.promises.readFile(fileUrl, options).then(b => JSON.parse(StringUtils.fromBuffer(b)));
+    /**
+     * Chargement d'un fichier json en objet
+     * @param fileUrl
+     * @param options
+     */
+    public static loadJson(fileUrl: string, options: { encoding: string; flag?: string; } | string = 'utf8') {
+        return FS.promises.readFile(fileUrl, options).then(b => JSON.parse(StringUtils.fromBuffer(b)));
+    }
+
+    /**
+     * Ecriture d'un fichier depuis des données json
+     * @param fileUrl
+     * @param data
+     */
+    public static writeJson(fileUrl: string, data: any) {
+        return FS.promises.writeFile(fileUrl, JSON.stringify(data));
     }
 }
