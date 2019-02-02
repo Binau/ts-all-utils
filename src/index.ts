20e62f07314ab87b83a6eef64c08e41f4cce1feb
binau <aubin.roumillac@gmail.com>
2019-02-03 00:35:14 +0100

    Ajout du randomUtils

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-export {FileNodeUtils} from './node/file.node.utils';
 export {ArrayUtils} from './array.utils';
+export {RandomUtils} from './random.utils';
+export {FileNodeUtils} from './node/file.node.utils';
 export {StringUtils} from './string.utils';
-
-
-
